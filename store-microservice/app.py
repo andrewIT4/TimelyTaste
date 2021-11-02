1bd964d2373b3e88f78cc007990708ef4415a38c
Chris Wong <yuhu198275@gmail.com>
2021-11-02 21:04:04 +0800

    fix bug for app.py

--- store-microservice/app.py
+++ store-microservice/app.py
@@ -121,3 +121,6 @@ def delete_store(store_id):
 def error_405(error):
     response = dict(error="Error")
     return jsonify(response), 405
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=15000, debug=True)
